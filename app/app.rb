1cab146431a60633264ec1fd4b673935d9507054
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-04-09 14:16:01 +0100

    added setup folders, rspec, gemfile

---
+++ app/app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+class Bookmark < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, 'key'
+  end
+  set :root, File.dirname(__FILE__)
+  run! if app_file == $0
+
+  get '/' do
+    erb(:index)
+  end
+
+end
